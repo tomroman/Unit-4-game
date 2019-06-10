20cad45d3550f2121128093f19000001962bf27a
Thomas Roman <thomasroman32@yahoo.com>
2019-06-09 23:17:30 -0500

    changed the image classes to crystal and added id matching color as well. Also added a section div in index.html. Added the reset function to the crystal onclick if statement. changed the val in crystalPoints function so that the id's were closed off in parenthesis

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -5,16 +5,17 @@ let totalPoints = 0;
 let wins = 0;
 let losses = 0;
 
-$("#rand-num").hmtl("<h2> + randomNumber + </h2> ");
+$("#random-number").html("<h2>" + randomNumber + "</h2>");
 
 $(".crystal").on("click", function () {
     totalPoints += parseInt(this.value);
-    $("total-score").text(totalPoints);
+    $("#total-score").text(totalPoints);
 
     if (totalPoints === randomNumber) {
         wins ++;
         $("#wins").text("Wins: " + wins);
         $("#game-result").text("You Win!");
+        reset();
         
     }
 
@@ -25,3 +26,26 @@ $(".crystal").on("click", function () {
         reset();
     }
 })
+
+function crystalPoints () {
+    let redPoints = Math.ceil(Math.random() * 12 );
+    let bluePoints = Math.ceil(Math.random() * 12 );
+    let yellowPoints = Math.ceil(Math.random() *12 );
+    let greenPoints = Math.ceil(Math.random() *12 );
+
+    $("#red-crystal").val(redPoints);
+    $("#blue-crystal").val(bluePoints);
+    $("#yellow-crystal").val(yellowPoints);
+    $("#green-crystal").val(greenPoints);
+
+}
+
+function reset () {
+    totalPoints = 0;
+    $("#total-score").text(totalPoints);
+    randomNumber = Math.floor(Math.random() * 102 ) + 19;
+    $("#random-Number").html("<h2>") + randomNumber + "</h2>"; 
+    crystalPoints();
+}
+
+crystalPoints();
